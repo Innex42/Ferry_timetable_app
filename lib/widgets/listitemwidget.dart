e64459057b469c6fb81b442387d629527e837313
Innex42 <innex4@gmail.com>
2023-04-19 21:53:08 +0100

    changed wind parameter measuring system and red Icon to cross

--- lib/widgets/listitemwidget.dart
+++ lib/widgets/listitemwidget.dart
@@ -5,6 +5,18 @@ import 'package:ferry_app/models/weatherdatamodel.dart';
 Widget customListView(BuildContext context, List<FerryTimes>? ferryTimes,
     List<WeatherData>? selectedWeatherData) {
   //List iconsTest = [9, 35, 46];
+  List highWindValue = [];
+  for (var i = 0; i < selectedWeatherData!.length; i++) {
+    if (double.parse(selectedWeatherData[i].windgust) >
+        double.parse(selectedWeatherData[i].windspeed)) {
+      highWindValue.add(double.parse(selectedWeatherData[i].windgust));
+    } else if (double.parse(selectedWeatherData[i].windspeed) >
+        double.parse(selectedWeatherData[i].windgust)) {
+      highWindValue.add(double.parse(selectedWeatherData[i].windspeed));
+    } else {
+      highWindValue.add(double.parse(selectedWeatherData[i].windgust));
+    }
+  }
   return ListView.builder(
       itemCount: ferryTimes!.length,
       itemBuilder: (context, index) {
@@ -99,13 +111,10 @@ Widget customListView(BuildContext context, List<FerryTimes>? ferryTimes,
                 ),
                 Row(
                   children: [
-                    if (double.parse(selectedWeatherData![index].windgust) <
-                            30 &&
-                        double.parse(selectedWeatherData[index].windspeed) <
-                            30) ...[
+                    if (highWindValue[index] < 30) ...[
                       Tooltip(
                         message:
-                            "The is likelihood of weather related disruptions is low.",
+                            "The likelihood of weather related disruptions is low.",
                         triggerMode: TooltipTriggerMode.tap,
                         showDuration: const Duration(seconds: 5),
                         child: Icon(
@@ -115,17 +124,11 @@ Widget customListView(BuildContext context, List<FerryTimes>? ferryTimes,
                         ),
                       )
                     ],
-                    if (double.parse(selectedWeatherData[index].windgust) >=
-                                30 &&
-                            double.parse(selectedWeatherData[index].windgust) <
-                                46 ||
-                        double.parse(selectedWeatherData[index].windspeed) >=
-                                30 &&
-                            double.parse(selectedWeatherData[index].windspeed) <
-                                46) ...[
+                    if (highWindValue[index] >= 30 &&
+                        highWindValue[index] < 46) ...[
                       Tooltip(
                         message:
-                            "The is a heightened risk of disruption to this service due to either Wind Speeds of ${selectedWeatherData[index].windspeed}mph or Gusts of ${selectedWeatherData[index].windgust}mph",
+                            "There is a heightened risk of disruption to this service due to Average Wind Speeds of ${selectedWeatherData[index].windspeed}mph or Gusts of ${selectedWeatherData[index].windgust}mph",
                         triggerMode: TooltipTriggerMode.tap,
                         showDuration: const Duration(seconds: 5),
                         child: Icon(
@@ -135,17 +138,14 @@ Widget customListView(BuildContext context, List<FerryTimes>? ferryTimes,
                         ),
                       )
                     ],
-                    if (double.parse(selectedWeatherData[index].windgust) >=
-                            46 ||
-                        double.parse(selectedWeatherData[index].windspeed) >=
-                            46) ...[
+                    if (highWindValue[index] >= 46) ...[
                       Tooltip(
                         message:
-                            "The is a strong likelihood of disruption or cancellations to this service due to either Wind Speeds of ${selectedWeatherData[index].windspeed}mph and Gusts of ${selectedWeatherData[index].windgust}mph",
+                            "There is a strong likelihood of disruption or cancellations to this service due to Average Wind Speeds of ${selectedWeatherData[index].windspeed}mph and Gusts of ${selectedWeatherData[index].windgust}mph",
                         triggerMode: TooltipTriggerMode.tap,
                         showDuration: const Duration(seconds: 5),
                         child: Icon(
-                          Icons.error,
+                          Icons.cancel,
                           color: Colors.red,
                           size: 50,
                         ),
@@ -160,9 +160,14 @@ Widget customListView(BuildContext context, List<FerryTimes>? ferryTimes,
                     SizedBox(
                       width: 30,
                     ),
-                    Text(ferryTimes[index].duration,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20))
+                    Tooltip(
+                      message: "Sailing Duration",
+                      triggerMode: TooltipTriggerMode.tap,
+                      showDuration: const Duration(seconds: 5),
+                      child: Text(ferryTimes[index].duration,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    )
                   ],
                 ),
               ],
